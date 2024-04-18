41ddd1db7b7596225f90e2fecf755a1c3b440e05
Jonathan <100314898+johnkryptochain@users.noreply.github.com>
2024-04-18 12:28:31 +0200

    Delete ash directory

--- ash/webui/jema_assistant_app_ui/url_constants.cc
+++
@@ -1,8 +0,0 @@
-#include "ash/webui/jema_assistant_app_ui/url_constants.h"
-
-namespace ash {
-
-const char kChromeUIJemaAssistantAppHost[] = "jemaos-ai";
-const char kChromeUIJemaAssistantAppURL[] = "chrome://jemaos-ai";
-
-} // namespace ash

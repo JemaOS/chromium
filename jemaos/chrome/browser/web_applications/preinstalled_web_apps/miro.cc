c3a850169460932b769ca2b30be7a34c5c6f514e
Jonathan <100314898+johnkryptochain@users.noreply.github.com>
2025-07-28 20:27:26 +0200

    Add files via upload

---
+++ jemaos/chrome/browser/web_applications/preinstalled_web_apps/miro.cc
@@ -0,0 +1,25 @@
+// Copyright 2025 Jema Technology. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "jemaos/chrome/browser/web_applications/preinstalled_web_apps/miro.h"
+
+#include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
+#include "jemaos/switches/urls/urls_constants.h"
+
+namespace web_app {
+
+ExternalInstallOptions GetConfigForMiro() {
+  ExternalInstallOptions options(
+      /*install_url=*/GURL(jemaos::constants::kMiroURL),
+      /*user_display_mode=*/mojom::UserDisplayMode::kStandalone,
+      /*install_source=*/ExternalInstallSource::kExternalDefault);
+
+  // Support all user types
+  options.user_type_allowlist = {"unmanaged", "managed", "child"};
+  options.add_to_quick_launch_bar = true;
+
+  return options;
+}
+
+}  // namespace web_app

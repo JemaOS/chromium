58ca65f3ba1484d0521f5a4e645bd1fdb00d26a0
ranjithkumar <rjnithintech@gmail.com>
2026-01-19 22:17:28 +0530

    [New] updated the chrome extension url

--- jemaos/switches/services/services_switches.cc
+++ jemaos/switches/services/services_switches.cc
@@ -29,7 +29,7 @@ const char kJemaOSAppsGalleryUpdateURL[] = "jemaos-apps-gallery-update-url";
 const char kJemaOSAssistantWebUrl[] = "jemaos-ai-url";
 
 const char kJemaOSStoreComPrefix[] = "https://store.jemaos.com";
-const char kJemaOSStoreIoPrefix[] = "https://store.jematechnology.fr";
+const char kJemaOSStoreIoPrefix[] = "https://chromewebstore.google.com";
 
 #if BUILDFLAG(JEMAOS_DEVICE)
 const char kJemaOSProductWarrantyURL[] = "jemaos-product-warranty-url";
